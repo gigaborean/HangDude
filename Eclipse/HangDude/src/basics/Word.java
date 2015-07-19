17ff0b3730931905ca057996aff66e06998e195a
Matthew Ceo <matthewceo@hotmail.com>
2015-07-19 14:28:28 +0100

    Overriding java.lang.Object. toString System request will return wordString from String method

--- Eclipse/HangDude/src/basics/Word.java
+++ Eclipse/HangDude/src/basics/Word.java
@@ -1,5 +1,17 @@
 package basics;
 
 public class Word {
+	 private String wordString;
+	 
+	 public Word(String wordString) {
+	  this.wordString = wordString;
+	 }
+	 
+	 public String toString() {
+		  return wordString;
+		 }
 
-}
+	 public String getWordString() {
+	  return wordString;
+	 }
+	}
---
+++ Eclipse/HangDude/src/main/Test.java
@@ -0,0 +1,13 @@
+package main;
+
+import basics.Word;
+
+public class Test {
+
+	public static void main(String[] args) {Word word = new Word("John is pretty good at this");
+	System.out.println(word);
+		// TODO Auto-generated method stub
+
+	}
+
+}
